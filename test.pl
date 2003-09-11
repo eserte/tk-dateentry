de1b73238de9077a17b52c9c63e3946689aeecfa
eserte <eserte>
2003-09-11 21:20:59 +0000

    BATCH is now default automatic test

--- test.pl
+++ test.pl
@@ -1,11 +1,13 @@
 #!/usr/bin/perl 
 #
 #
-BEGIN { $| = 1; print "1..2\n"; }
+BEGIN { $| = 1; print "1..2\n"; $^W = 1 }
 END {print "not ok 1\n" unless $loaded;}
 use Tk;
 use Tk::DateEntry;
 $loaded = 1;
+
+if (!defined $ENV{BATCH}) { $ENV{BATCH} = 1 }
 
 print "ok 1\n";
 
@@ -34,17 +36,38 @@ static char arrowdownwin2_bits[] = {
 EOF
 
 $mw->Label(-text=>'Select date:')->pack(-side=>'left');
-$mw->DateEntry(-textvariable => \$date,
-	       -todaybackground => "green",
-	       -arrowimage => $arrowdownwin,
-	       @extra_args,
-	      )->pack(-side=>'left');
+my $de = $mw->DateEntry(-textvariable => \$date,
+			-todaybackground => "green",
+			-arrowimage => $arrowdownwin,
+			@extra_args,
+		       )->pack(-side=>'left');
 
-$mw->Button(-text => 'OK',
-	    -command => sub {
-		print "Selected date: $date\n";
-		$mw->destroy;
-	    })->pack;
-
-MainLoop unless $ENV{BATCH};
+if ($ENV{BATCH}) {
+    $mw->update; $mw->after(200);
+    # The used members are internals, do not use in regular programs!
+    $mw->after(200, sub {
+		   $de->{_backbutton}->invoke;
+	       });
+    $mw->after(700, sub {
+		   $de->{_nextbutton}->invoke;
+	       });
+    $mw->after(1200, sub {
+		   $de->{_daybutton}->[2]->[3]->invoke;
+	       });
+    $de->buttonDown;
+    $mw->update;
+    # This blocks until a date is clicked
+    if (!defined $date || $date !~ /\d/) {
+	print "not ";
+    }
+    print "ok 2\n";
+} else {
+    $mw->Button(-text => 'OK',
+		-command => sub {
+		    print "# Selected date: $date\n";
+		    print "ok 2\n";
+		    $mw->destroy;
+		})->pack;
+    MainLoop;
+}
 
